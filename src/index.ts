00770e864b0c7f4452465d6ecb03ea604a7ef6da
Matthew Little <zone117x@gmail.com>
2020-10-19 19:18:33 +0200

    chore: simplify queries

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const STX_TOTAL_TXS_AT_BLOCK_QUERY = `
       FROM accounts 
       WHERE type = 'STACKS' 
       AND address !~ '(-|_)' 
-      AND length(address) BETWEEN 33 AND 34 
+      AND length(address) BETWEEN 33 AND 34
       AND lock_transfer_block_id <= $1
       ORDER BY address, block_id DESC, vtxindex DESC 
   )
@@ -17,24 +17,8 @@ const STX_TOTAL_TXS_AT_BLOCK_QUERY = `
   ) AS val FROM totals
 `;
 
-
-const STX_LATEST_TOTAL_TXS_QUERY = `
-  WITH 
-    block_height AS (SELECT MAX(block_id) from accounts),
-    totals AS (
-      SELECT DISTINCT ON (address) credit_value, debit_value 
-        FROM accounts 
-        WHERE type = 'STACKS' 
-        AND address !~ '(-|_)' 
-        AND length(address) BETWEEN 33 AND 34 
-        AND lock_transfer_block_id <= (SELECT * from block_height) 
-        ORDER BY address, block_id DESC, vtxindex DESC 
-    )
-  SELECT (SELECT * from block_height) AS val
-  UNION ALL
-  SELECT SUM(
-    CAST(totals.credit_value AS bigint) - CAST(totals.debit_value AS bigint)
-  ) AS val FROM totals
+const STX_LATEST_BLOCK_HEIGHT_QUERY = `
+  SELECT MAX(block_id) as block_id FROM accounts
 `;
 
 const STX_VESTED_BY_BLOCK_QUERY = `
@@ -54,9 +38,9 @@ async function run() {
   const client = new Client();
   await client.connect()
 
-  const initSupplyRes = await client.query<{val: string}>(STX_LATEST_TOTAL_TXS_QUERY);
-  const initBlockHeight = parseInt(initSupplyRes.rows[0].val);
-  const initStxSupply = BigInt(initSupplyRes.rows[1].val);
+  const currentBlockHeight = (await client.query<{block_id: number}>(STX_LATEST_BLOCK_HEIGHT_QUERY)).rows[0].block_id;
+  const res = await client.query<{val: string}>(STX_TOTAL_TXS_AT_BLOCK_QUERY, [currentBlockHeight]);
+  const currentStxSupply = BigInt(res.rows[0].val);
 
   // block heights where vesting stx unlock (and become liquid)
   const vestedByBlockRes = await client.query<{block_id: string, micro_stx: string;}>(STX_VESTED_BY_BLOCK_QUERY);
@@ -75,7 +59,7 @@ async function run() {
 
   // final unlocked supplies by block height
   const totals: { block_height: number; queried_micro_stx: bigint; vested_micro_stx: bigint; total_calculated: bigint }[] = [];
-  for (let blockHeight = initBlockHeight; blockHeight < lastBlockHeight; blockHeight++) {
+  for (let blockHeight = currentBlockHeight; blockHeight < lastBlockHeight; blockHeight++) {
     const total = { block_height: blockHeight, queried_micro_stx: 0n, vested_micro_stx: 0n, total_calculated: 0n };
     total.vested_micro_stx = totals[totals.length - 1]?.vested_micro_stx ?? 0n;
     const vestingBlock = vestingResults.find(r => r.block_height === blockHeight);
@@ -85,8 +69,8 @@ async function run() {
     if (lockTransferBlockHeights.includes(blockHeight)) {
       const res = await client.query<{val: string}>(STX_TOTAL_TXS_AT_BLOCK_QUERY, [blockHeight]);
       total.queried_micro_stx = BigInt(res.rows[0].val);
-    } if (total.block_height === initBlockHeight) {
-      total.queried_micro_stx = initStxSupply;
+    } if (total.block_height === currentBlockHeight) {
+      total.queried_micro_stx = currentStxSupply;
     } else {
       // no change in unlocked balances so use previous block's
       total.queried_micro_stx = totals[totals.length - 1].queried_micro_stx;
@@ -96,7 +80,7 @@ async function run() {
   }
 
   // sanity checks
-  assert(totals[0].block_height === initBlockHeight, 'unexpected initial block height');
+  assert(totals[0].block_height === currentBlockHeight, 'unexpected initial block height');
   for (let i = 1; i < totals.length; i++) {
     try {
       assert(totals[i].block_height === totals[i - 1].block_height + 1, 'unordered blocks!');
@@ -109,12 +93,12 @@ async function run() {
 
   // output to csv
   const fd = fs.openSync('supply.csv', 'w');
-  fs.writeSync(fd, 'block_height,unlocked_micro_stx,unlocked_stx,vested\r\n');
+  fs.writeSync(fd, 'block_height,unlocked_micro_stx,unlocked_stx\r\n');
   for (const entry of totals) {
     const stxInt = entry.total_calculated.toString().slice(0, -6);
     const stxFrac = entry.total_calculated.toString().slice(-6);
     const stxStr = `${stxInt}.${stxFrac}`;
-    fs.writeSync(fd, `${entry.block_height},${entry.total_calculated},${stxStr},${entry.vested_micro_stx}\r\n`);
+    fs.writeSync(fd, `${entry.block_height},${entry.total_calculated},${stxStr}\r\n`);
   }
   fs.closeSync(fd);
 
